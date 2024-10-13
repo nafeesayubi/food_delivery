98a291970b11112c8182cafb69c93a1ec70a69dc
nafeesayubi <nafeesayubi1620@gmail.com>
2024-10-13 05:56:27 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+import 'package:food_delivery/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
+
+import 'pages/login_page.dart';
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext Context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LoginPage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+    );
+  }
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,36 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.lock_open_rounded,
+              size: 100,
+              color: Theme.of(context).colorScheme.inversePrimary,
+            ),
+
+            const SizedBox(height : 25),
+
+            Text(
+              "Food Delivery App",
+              style: TextStyle(
+                fontSize: 16,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
